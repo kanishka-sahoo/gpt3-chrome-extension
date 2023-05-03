a3849799e8756f60b9cf519894881c14b2c62815
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2023-05-03 20:27:44 +0530

    Initial commit for new branch

--- popup.js
+++ popup.js
@@ -23,22 +23,25 @@ form.addEventListener('submit', async (event) => {
   // Store the API token in storage
   chrome.storage.sync.set({ apiToken: apiToken });
   try {
-    const response = await fetch('https://api.openai.com/v1/completions', {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer '+apiToken
       },
       body: JSON.stringify({
-        model: "text-davinci-003",
-        prompt: `Question: ${question}\nAnswer:`,
-        max_tokens: 1000,
+        model: "gpt-3.5-turbo",
+        messages: [
+          {"role": "system", "content": "You are a kind and helpful AI."},
+          {"role": "user", "content": question}
+        ], 
+        max_tokens: 3000,
         temperature: 0.7,
         n: 1
       })
     });
     const data = await response.json();
-    const answer = data.choices[0].text;
+    const answer = data.choices[0].message.content;
     answerDiv.textContent = answer;
     loaderDiv.style.display = 'none';
   } catch (error) {
